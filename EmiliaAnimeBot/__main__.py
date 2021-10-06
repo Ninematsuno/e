3a8c7bcdcb8a54e2b67d3f3ba02507257d956fd1
Cypher Izumi <85168398+IzumiCypherX@users.noreply.github.com>
2021-10-06 11:14:00 +0530

    probable fix

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -82,7 +82,10 @@ buttons = [
 ]
 
 HELP_STRINGS ="""
-This is a test by izumi
+`Hey there!` My name is [Minato namikaze](https://telegra.ph/file/c64faeb5ca0f9885c8ab1.jpg) 
+━━━━━━━━━━━━━━━━━━━━━━━━\nTʀᴜsᴛ  In my power\n━━━━━━━━━━━━━━━━━━━━━━━━\nMain commands available:\n ❍ `/start`: Cʜᴇᴄᴋ Mᴇ... Iꜰ I Aᴍ Aʟɪᴠᴇ Oʀ Nᴏᴛ\n ❍ `/help`: Usᴇ Tʜɪs... Iꜰ Yᴏᴜ Nᴇᴇᴅ Mᴏʀᴇ Iɴꜰᴏ Aʙᴏᴜᴛ Mᴇ!\n
+━━━━━━━━━━━━━━━━━━━━━━━━\n
+For Issues Report At @minato_support\n━━━━━━━━━━━━━━━━━━━━━━━━\nAll commands can either be used with / or !. 
 """
 
 IMPORTED = {}
