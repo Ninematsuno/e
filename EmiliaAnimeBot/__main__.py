5bf2f05bef54850e2ef50687b65e57115a1119a1
Nine <89542501+Ninematsuno@users.noreply.github.com>
2021-10-06 17:54:45 +0530

    Update __main__.py

--- EmiliaAnimeBot/__main__.py
+++ EmiliaAnimeBot/__main__.py
@@ -61,11 +61,10 @@ PM_START_TEXT ="""
 [∆𝕴 𝖆𝖒 𝕸𝖎𝖓𝖆𝖙𝖔 𝖆𝖐𝖆 𝖀𝖗 𝖄𝖊𝖑𝖑𝖔𝖜 𝖋𝖑𝖆𝖘𝖍 ∆](https://telegra.ph/file/af3b555514ee92666e95d.jpg)
 ━━━━━━━━━━━━━━━━━━━━━━
 This is a highly featured weebish style group management
-
 Press /help to see what this pro bot can do
 ━━━━━━━━━━━━━━━━━━━━━━
-•Since `{}` , 
-°I am protecting `{}` users
+⦿Since `{}` , 
+⦿I am protecting `{}` users
 “𝑯𝒆𝒓𝒆 𝑰 𝒂𝒎 𝒓𝒆𝒂𝒅𝒚 𝒕𝒐 𝒉𝒆𝒍𝒑 𝒚𝒐𝒖 𝒎𝒂𝒏𝒂𝒈𝒆 𝒚𝒐𝒖𝒓 𝒈𝒓𝒑𝒔 𝒆𝒂𝒔𝒊𝒍𝒚...."
 """
 
@@ -84,11 +83,6 @@ buttons = [
 
 HELP_STRINGS ="""
 hey there! Minato namikaze  again
-
-Main commands available:
- ❍ /start: Cʜᴇᴄᴋ Mᴇ... Iꜰ I Aᴍ Aʟɪᴠᴇ Oʀ Nᴏᴛ
-
- For Issues Report At @minato_support
 """
 
 IMPORTED = {}
